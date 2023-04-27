b84f04d88ab992a6ade0a5e2ff7ac1bfc45b2a9a
Wellington Henrique <wellingtonhlc@gmail.com>
2023-04-26 21:54:07 -0300

    Criando endpoint Snacks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Prisma, PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response} from 'express';
 
@@ -6,12 +7,30 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
+const prisma = new PrismaClient();
+
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     const { message } =  req.body;
 
     res.send({message: message});
+});
+
+app.get('/snacks', async (req: Request, res: Response) => {
+    const { snack } =  req.query;
+
+    if(!snack) return res.status(400).send({error: "Snack is required"});
+
+    const snacks = await prisma.snack.findMany({
+        where: {
+            snack: {
+                equals: snack as string
+            }
+        }
+    })
+
+    res.send(snacks);
 });
 
 app.listen(port, () => {
